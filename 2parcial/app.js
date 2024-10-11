98de5ae75ff4a1d98fb21a1e445a8b4176834ca4
Uriel Reyna <163452122+LeHambu@users.noreply.github.com>
2024-10-10 23:55:38 -0500

    Add files via upload

--- 2parcial/app.js
+++ 2parcial/app.js
@@ -1,9 +1,42 @@
-import { generarNumeroAleatorio, mostrarNumeros, ordenarAscendente, ordenarDescendente } from './numeros.js';
+
+function generarNumeroAleatorio(numeros) {
+    if (numeros.length >= 99) return;
+
+    let numeroAleatorio;
+    do {
+        numeroAleatorio = Math.floor(Math.random() * 99) + 1;
+    } while (numeros.includes(numeroAleatorio));
+
+    numeros.push(numeroAleatorio);
+}
+
+
+function mostrarNumeros(numeros) {
+    const container = document.getElementById('number-container');
+    container.innerHTML = '';
+
+    numeros.forEach(numero => {
+        const box = document.createElement('div');
+        box.classList.add('box');
+        box.textContent = numero;
+        container.appendChild(box);
+    });
+}
+
+
+function ordenarAscendente(numeros) {
+    numeros.sort((a, b) => a - b);
+}
+
+
+function ordenarDescendente(numeros) {
+    numeros.sort((a, b) => b - a);
+}
+
 
 const numeros = [];
 
 document.getElementById('btn-generar').addEventListener('click', () => {
-    console.log('click');
     generarNumeroAleatorio(numeros);
     mostrarNumeros(numeros);
 });
